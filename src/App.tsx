54e07c80c34da93cc1debf2f48473c16eb676796
sihyun10 <hyhy200170@gmail.com>
2026-04-22 16:35:12 +0900

    chore: Shadcn/ui 세팅

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,13 @@
 import "./App.css";
+import { Button } from "./components/ui/button";
 
 function App() {
-  return <div className="font-bold underline">Hello DADO</div>;
+  return (
+    <div className="font-bold underline">
+      Hello DADO
+      <Button>입장하기</Button>
+    </div>
+  );
 }
 
 export default App;
