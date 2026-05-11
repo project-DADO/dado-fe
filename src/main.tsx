a306966b0c6c61f1e7e906ca6435f0f16b0fbf47
xlouisjungx <cutaandcrazy@gmail.com>
2026-05-11 20:32:46 +0900

    Feature: 메인페이지를 구현하였습니다. 달마다 달력을 볼 수 있습니다. 추가적으로, 더보기 버튼에 동작을 추가하여 구현하였습니다. #1

--- src/main.tsx
+++ src/main.tsx
@@ -1,6 +1,8 @@
 import { createRoot } from "react-dom/client";
-import "./index.css";
+//import "./index.css";
 import App from "./App.tsx";
+//import mainPage from "./pages/mainPage.tsx";
+import "./css/mainPage.css";
 import { BrowserRouter } from "react-router";
 
 createRoot(document.getElementById("root")!).render(
