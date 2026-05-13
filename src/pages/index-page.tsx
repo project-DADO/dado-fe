b41639355e746e9c6689ed00cbf0bf11df200673
xlouisjungx <cutaandcrazy@gmail.com>
2026-05-13 20:01:34 +0900

    필요없는 파일 및 사이트 첫 방문시 나타나는 페이지 수정

--- src/pages/index-page.tsx
+++
@@ -1,3 +0,0 @@
-export default function IndexPage() {
-  return <div>DADO 메인 홈페이지</div>;
-}
--- src/root-route.tsx
+++ src/root-route.tsx
@@ -1,12 +1,12 @@
 import { Navigate, Route, Routes } from "react-router";
-import IndexPage from "./pages/index-page";
+import MainPage from "./pages/mainPage";
 import GlobalLayout from "./components/layout/global-layout";
 
 export default function RootRoute() {
   return (
     <Routes>
       <Route element={<GlobalLayout />}>
-        <Route path="/" element={<IndexPage />} />
+        <Route path="/" element={<MainPage />} />
 
         <Route path="*" element={<Navigate to={"/"} />} />
       </Route>
